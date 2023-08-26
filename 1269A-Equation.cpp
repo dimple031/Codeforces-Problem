62e2954c5f06a8da7fff9bd0ce36aa6379b9bd50
dimple031 <113361027+dimple031@users.noreply.github.com>
2023-08-26 14:58:39 +0530

    Create 1269A-Equation.cpp

---
+++ 1269A-Equation.cpp
@@ -0,0 +1,9 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+    int main(){
+        int n; cin>>n;
+        if(n==1)
+        cout<<9<<" "<<8<<endl;
+        else
+        cout<<3*n<<" "<<2*n<<endl;
+    }
